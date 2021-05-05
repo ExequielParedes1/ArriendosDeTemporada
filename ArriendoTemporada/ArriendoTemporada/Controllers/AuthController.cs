db46b6285733c694c18d3ba0ddefc262d72b0853
alanbustosm <alan.bustos@alumnos.duoc.cl>
2021-05-04 22:37:14 -0400

    CRUD
    list
    insert
    delete

---
+++ ArriendoTemporada/ArriendoTemporada/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ArriendoTemporada.Negocio;
+using System.Web.Security;
+
+namespace ArriendoTemporada.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Cliente cliente, string ReturnUrl)
+        {
+            if (IsValid(cliente))
+            {
+                FormsAuthentication.SetAuthCookie(cliente.Rut_Cliente, false);
+                if(ReturnUrl != null)
+                {
+                    return Redirect(ReturnUrl);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            TempData["mensaje"] = "Nombre o usuario Incorrectos";
+            return View(cliente);
+                
+        }
+
+        private bool IsValid(Cliente cliente)
+        {
+            return cliente.Autenticar();
+        }
+
+        public ActionResult LogOut ()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ArriendoTemporada/ArriendoTemporada/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArriendoTemporada.MvcApplication" Language="C#" %>
