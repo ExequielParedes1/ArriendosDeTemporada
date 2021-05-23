3683ad89612e9af45930e30473c1747547546f91
alanbustosm <alan.bustos@alumnos.duoc.cl>
2021-05-22 20:45:39 -0400

    update 22/05

---
+++ .net/ArriendoTemporada/ArriendoTemporada.Negocio/Reserva_Servicio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArriendoTemporada.DALC;
+
+namespace ArriendoTemporada.Negocio
+{
+    public class Reserva_Servicio
+    {
+
+        public decimal Cupos { get; set; }
+        public Reserva Reserva { get; set; }
+        public Servicio Servicio { get; set; }
+
+        ArriendoTemporadaEntities db = new ArriendoTemporadaEntities();
+
+
+        public List<Reserva> ReadId(decimal cliente_id)
+        {
+            return db.RESERVA_SERVICIO.Select(rs => new Reserva_Servicio()
+            {
+                Cupos = rs.CANTIDAD_CUPO,
+                Reserva = new Reserva()
+                {
+                    Id = rs.RESERVA.ID_RESERVA,
+                    Fecha_Inicio = rs.RESERVA.FECHA_INICIO,
+                    Fecha_Fin = rs.RESERVA.FECHA_FIN,
+                    Estado = rs.RESERVA.ESTADO,
+                    Cliente_Id = rs.RESERVA.CLIENTE_ID_CLIENTE
+
+                },
+                Servicio = new Servicio()
+                {
+                    Id_Servicio = rs.SERVICIO.ID_SERVICIO,
+
+
+                }
+            }).Where(r => r.Cliente_Id == cliente_id).ToList();
+
+        }
+
+
+
+
+
+
+
+    }
+}
